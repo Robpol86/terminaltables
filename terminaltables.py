663e0c5c23e61c98ba56e392d8002187184ce4fd
Robpol86 <robpol86@gmail.com>
2014-09-02 17:56:06 -0700

    Clearer attributes, started on table property.

--- terminaltables.py
+++ terminaltables.py
@@ -30,38 +30,79 @@ class AsciiTable(object):
         self.table_data = table_data
         self.title = title
 
-        self.first_row_heading = False
-        self.justify_right = False
-        self.no_inner_border = False
-        self.no_outer_border = False
-        self.partial_border = False
+        self.inner_column_border = True
+        self.inner_heading_row_border = False
+        self.inner_row_border = True
+        self.justify_columns = dict()  # {0: 'right', 1: 'left', 2: 'center'}
+        self.outer_border = False
         self.padding_bottom = 0
         self.padding_left = 1
         self.padding_right = 1
         self.padding_top = 0
 
+    def column_max_width(self, column_number):
+        """TODO
+
+        Positional arguments:
+        column_number -- the column number to query.
+
+        Returns:
+        The max width of the column (integer).
+        """
+        raise NotImplementedError('todo')
+
     @property
     def column_widths(self):
-        padded_table_data = self.padded_table_data
-        if not padded_table_data:
+        if not self.table_data:
             return []
-        return [len(c) for c in padded_table_data[0]]
+
+        number_of_columns = max(len(r) for r in self.table_data)
+        widths = [0] * number_of_columns
+
+        for row in self.table_data:
+            for i in range(row):
+                widths[i] = max(widths[i], len(row[i]))
+
+        return widths
 
     @property
     def padded_table_data(self):
         if not self.table_data:
             return []
+
+        # Set all rows to the same number of columns.
         max_columns = max(len(r) for r in self.table_data)
         new_table_data = [r + [''] * (max_columns - len(r)) for r in self.table_data]
+
+        # Pad strings in each cell, and apply text-align/justification.
+        column_widths = self.column_widths
+        for row in new_table_data:
+            for i in range(row):
+                justification = self.justify_columns.get(i, 'right')
+                if justification == 'right':
+                    cell = row[i].rjust(column_widths[i])
+                elif justification == 'center':
+                    cell = row[i].center(column_widths[i])
+                else:
+                    cell = row[i].ljust(column_widths[i])
+                row[i] = cell
+
         return new_table_data
 
     @property
+    def table(self):
+        inner_column_border = '{l}{c}{r}'.format(c=(self.CHAR_VERTICAL if self.inner_column_border else ''),
+                                                 l=(' ' * self.padding_left), r=(' ' * self.padding_right))
+        table_data = [r.join(inner_column_border) for r in self.padded_table_data]
+        raise NotImplementedError
+
+    @property
     def terminal_height(self):
-        return struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))[0]
+        return struct.unpack('hhhh', fcntl.ioctl(0, termios.TIOCGWINSZ, '\000' * 8))[0]
 
     @property
     def terminal_width(self):
-        return struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))[1]
+        return struct.unpack('hhhh', fcntl.ioctl(0, termios.TIOCGWINSZ, '\000' * 8))[1]
 
 
 class UnixTable(AsciiTable):
