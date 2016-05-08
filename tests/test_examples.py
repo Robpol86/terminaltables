8c8a70e31e733d771047b05e6450d99535883d35
Robpol86 <robpol86@gmail.com>
2016-05-08 11:13:42 -0700

    Pinning linters, Travis deployments, tox env vars.
    Pinning linter versions to prevent test failures after no changes.
    
    Having Travis deploy to PyPI instead of me doing it manually.
    
    Moving PYTHONIOENCODING into tox.ini to simplify subprocess tests.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -13,9 +13,9 @@ except ImportError:
 
 @pytest.mark.parametrize('suffix', range(1, 4))
 def test(suffix):
-    """Test with subprocess."""
+    """Test with subprocess.
+
+    :param int suffix: Numerical suffix of file name to test.
+    """
     path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'example{0}.py'.format(suffix)))
-    env = dict(PYTHONIOENCODING='utf-8')
-    if 'SystemRoot' in os.environ:
-        env['SystemRoot'] = os.environ['SystemRoot']
-    subprocess.check_output([sys.executable, path], env=env, stderr=subprocess.STDOUT)
+    subprocess.check_output([sys.executable, path], stderr=subprocess.STDOUT)
