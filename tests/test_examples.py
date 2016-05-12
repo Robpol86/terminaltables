47a2feff40b59ce2c5b18b04f46bc4892bb6f778
Robpol86 <robpol86@gmail.com>
2016-05-11 21:15:19 -0700

    Testing Python2.6 in AppVeyor.
    Eliminated subprocess32, just using Popen().
    
    Moving PYTHONIOENCODING from tox to Popen() since it's needed when box
    drawings chars are piped to subprocess but not when it runs on a
    console.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,12 @@
 """Test example scripts."""
 
+from __future__ import print_function
+
 import os
+import subprocess
 import sys
 
 import pytest
-
-try:
-    import subprocess32 as subprocess
-except ImportError:
-    import subprocess
 
 
 @pytest.mark.parametrize('suffix', range(1, 4))
@@ -18,4 +16,13 @@ def test(suffix):
     :param int suffix: Numerical suffix of file name to test.
     """
     path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'example{0}.py'.format(suffix)))
-    subprocess.check_output([sys.executable, path], stderr=subprocess.STDOUT)
+    env = dict(os.environ, PYTHONIOENCODING='utf-8')
+
+    # Run.
+    proc = subprocess.Popen([sys.executable, path], env=env, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    output = proc.communicate()[0]
+    try:
+        assert proc.poll() == 0
+    except AssertionError:
+        print(output)
+        raise
