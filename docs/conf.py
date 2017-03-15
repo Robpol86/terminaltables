61210a56560daaebde8ca68e5dfe7d09dae6eb87
Robpol86 <robpol86@gmail.com>
2017-03-14 17:15:07 -0700

    Showing time zone in docs.
    Also using latest GitHub LICENSE file for MIT.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import time
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 author = '@Robpol86'
 copyright = '{}, {}'.format(time.strftime('%Y'), author)
-html_last_updated_fmt = '%c'
+html_last_updated_fmt = '%c {}'.format(time.tzname[time.localtime().tm_isdst])
 master_doc = 'index'
 project = __import__('setup').NAME
 pygments_style = 'friendly'
